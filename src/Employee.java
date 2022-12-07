c4f074715d7e004c3aaeb7a399e40ee9178fca2e
lexxxxa82 <lexxxxa.82@mail.ru>
2022-12-07 12:16:53 +0800

    не могу коректно реализовать счетчик, при добавлении нового сотрудника нумерация продолжается дальше

--- src/Employee.java
+++ src/Employee.java
@@ -1,35 +1,52 @@
 public class Employee {
-     private final String id;
-     private String department;
-     private double salary;
+    private final String id;
+    private String department;
+    private double salary;
+    private final int obj;
+      private static int size;
 
-     public Employee (String id, String department, double salary) {
-         this.id = id;
-         this.department = department;
-         this.salary = salary;
-     }
+    public Employee ( String id, String department, double salary) {
+        this.obj = size++;
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+    }
 
-     public String getId() {
+    public String getId() {
         return id;
-     }
+    }
 
-     public String getDepartment(){
+    public String getDepartment(){
         return department;
-     }
+    }
 
-     public void setDepartment(String Department) {
-              this.department = Department;
-     }
-     public double getSalary() {
+    public void setDepartment(String Department) {
+        this.department = Department;
+    }
+    public double getSalary() {
         return  salary;
-     }
+    }
 
     public void setSalary(double salary) {
         this.salary = salary;
     }
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+         Employee c2 = (Employee) other;
+        return id.equals(c2.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(id);
+    }
 
     @Override
     public String toString() {
-        return this.id + " отдел: " + this.getDepartment() + " зарплата: " + this.getSalary();
+        return " номер п/п " + obj + " " + this.id + " отдел: " + this.getDepartment() + " зарплата: "
+                + this.getSalary() + "\n";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,19 @@ public class Main {
     public static void main(String[] args) {
         EmployeeData employeeData = new EmployeeData();
 
-        employeeData.addEmployee("Баранова Полина Ильинична", "1",2.23);
+        employeeData.addEmployee( "Баранова Полина Ильинична", "1",2.23);
         employeeData.addEmployee("Белоусов Артемий Дмитриевич", "2",14);
         employeeData.addEmployee("Горбунова Надежда Александровна", "3",13);
-        employeeData.addEmployee("Еремин Александр Никитич", "4",21);
+        employeeData.addEmployee("Еремин Александр Никитич", "4",15);
         employeeData.addEmployee("Крюкова Мария Викторовна", "5",8);
         employeeData.addEmployee("Кузнецова Кира Елисеевна", "1",19);
         employeeData.addEmployee("Фомин Максим Егоровича", "2",23);
         employeeData.addEmployee("Рябов Михаил Фёдорович", "3",25);
         employeeData.addEmployee("Савин Артемий Елисеевич", "4",17);
         employeeData.addEmployee("Казаков Лев Ильич", "5",15);
-       // employeeData.addEmployee("Крылова Вера Данииловна", "2",22400);
+        //employeeData.addEmployee("Крылова Вера Данииловна", "2",22400);
 
-        employeeData.printAllEmployees();
+        employeeData.printAllEmployees();//список Ф.И.О
         System.out.println();
         System.out.println(employeeData);
 
@@ -24,11 +24,15 @@ public class Main {
 
         employeeData.removeEmployee("Фомин Максим Егоровича");
         employeeData.findEmployee("Фомин Максим Егоровича");
+        employeeData.removeEmployee("Савин Артемий Елисеевич");
+        employeeData.findEmployee("Савин Артемий Елисеевич");
 
-        //employeeData.addEmployee("Крылова Вера Данииловна", "2",22);
-        employeeData.printAllEmployees();//список сотрудников
+        employeeData.addEmployee("Крылова Вера Данииловна", "2",2);
+        employeeData.addEmployee("Игумнов Алексей Николаевич", "2",28);
         System.out.println();
         System.out.println("Количество сотрудников: " + EmployeeData.getCurrentSize());
+        System.out.println(employeeData);
+
         EmployeeData.totalEmployeePay();//общие затраты на выплату  зарплат
         System.out.println();
         EmployeeData.averageStaffSalary();// средняя зарплата
@@ -38,3 +42,4 @@ public class Main {
         EmployeeData.minimumWageEmployeePrint();//сотрудник с минимальной зарплатой
     }
 }
+
